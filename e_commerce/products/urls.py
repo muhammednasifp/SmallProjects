62784427e9f0777f2f8f855abb1f6d342118da6b
muhammednasifp <muhammednasifp41@gmail.com>
2025-08-01 00:47:19 +0530

    did some touch up in template

--- e_commerce/products/urls.py
+++ e_commerce/products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('index/',views.index,name="index"),
+    path('index/',views.index,name="home"),
+    path('',views.index,name='home')
 ]
