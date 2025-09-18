0ea89ad129ea0d6fd72fdca543cd57a2be0f156c
muhammednasifp <muhammednasifp41@gmail.com>
2025-09-18 23:07:21 +0530

    ADDED DATA FROM ADMIN PANEL TO PRODUCT PAGE

--- e_commerce/products/views.py
+++ e_commerce/products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from . models import Product
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
 def list_product(request):
-    return render(request,'products.html')
+    product_list=Product.objects.all()
+    
+    return render(request,'products.html',{'products':product_list})
 
 def details_product(request):
     return render(request,'product_details.html')
