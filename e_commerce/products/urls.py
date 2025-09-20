f86b6c0fcca6d3e411e14bf992f061ef682c2176
muhammednasifp <muhammednasifp41@gmail.com>
2025-09-21 00:09:16 +0530

    completed User management

--- e_commerce/products/urls.py
+++ e_commerce/products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name='home'),
+    path('',views.home,name='home'),
     path('list_product',views.list_product,name='list_product'),
-    path('details_product',views.details_product,name='deatils_product'),
+    path('details_product/<pk>',views.details_product,name='details_product'),
 ]
--- e_commerce/products/views.py
+++ e_commerce/products/views.py
@@ -4,18 +4,27 @@ from django.core.paginator import Paginator
 #paginator is used to make different pages in the webpage
 # Create your views here.
 
-def index(request):
-    return render(request,'index.html')
+def home(request):
+    featured_pList=Product.objects.order_by('-priority')[:4]
+    latest_pList=Product.objects.order_by('priority')[:4]
+    context={'featured_List':featured_pList,
+             'latest_list':latest_pList
+            }
+    return render(request,'index.html',context)
+    
 
-def list_product(request):
+def list_product(request):      
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=Product.objects.all()
+    #product_list=Product.objects.all()
+    product_list=Product.objects.order_by('-priority')
     product_paginator=Paginator(product_list,2)
     #here paginator constructor created with two items
     product_list=product_paginator.get_page(page)
     return render(request,'products.html',{'products':product_list})
 
-def details_product(request):
-    return render(request,'product_details.html')
+def details_product(request,pk):
+    product_details=Product.objects.get(pk=pk)
+    context={'pDetails':product_details}
+    return render(request,'product_details.html',context)
